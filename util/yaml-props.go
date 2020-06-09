c6f4027c67f03e3314ea14cbe28e85edbb007824
M. Dresdner <mitchd@mindspring.com>
2020-06-09 13:50:57 -0400

    Clean up and add comments

--- util/yaml-props.go
+++ util/yaml-props.go
@@ -15,43 +15,45 @@ type Config struct {
 	}
 }
 
-func FetchYAML () (string, error) {
+func FetchYAML() (string, error) {
 
 	dat, err := ioutil.ReadFile("./properties/dev-properties.yaml")
 	if err != nil {
 		return "", err
 	}
-	//fmt.Print(string(dat))
 
+	// Unmarshal YAML file into Config struct
 	var config Config
 	err = yaml.Unmarshal([]byte(dat), &config)
 	if err != nil {
 		return "", err
 	}
 
-	fmt.Println("Shhh:", os.Getenv("SECRET"))
+	// Iterate over properties, decoding any secrets
 	for k, v := range config.Db.Pg {
-		// extract and decode strings wrapped in ![]
-		if strings.HasPrefix(v,"![") && strings.HasSuffix(v,"]") {
+		// Extract and decode secrets which are wrapped in ![] (like Mule)
+		if strings.HasPrefix(v, "![") && strings.HasSuffix(v, "]") {
 			secret := strings.TrimRight(strings.TrimLeft(v, "!["), "]")
+			// Convert hex representation of secret to uint8 byte array
 			data, err := hex.DecodeString(secret)
 			if err != nil {
 				return "", err
 			}
-			decoded, e := decrypt(data,[]byte(os.Getenv("SECRET")))
+			decoded, e := decrypt(data, []byte(os.Getenv("SECRET")))
 			if e != nil {
 				return "", fmt.Errorf("error: %v", e)
 			}
 
 			fmt.Printf("found %s\n", decoded)
-			// replace value with decoded secret in map
+			// Replace encoded map value with the decoded secret
 			v = string(decoded)
 			config.Db.Pg[k] = v
 		}
-		fmt.Printf("key %s: %s\n", k, v)
+		// fmt.Printf("key %s: %s\n", k, v)
 	}
 
-	// connStr := "user=goland dbname=goland password=goland host=localhost port=30432 sslmode=disable"
+	// Create the database connection string, this one is for Postgres
+	// ToDo: Create a general purpose function that handles other DB's
 	return fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable",
 		config.Db.Pg["user"], config.Db.Pg["dbname"], config.Db.Pg["password"],
 		config.Db.Pg["host"], config.Db.Pg["port"]), err
